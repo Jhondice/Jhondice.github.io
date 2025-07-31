1cfee0b73dc3faca9d3bc24500f2336b2f0bed84
Jhondice <ingjhonaimer@gmail.com>
2025-07-31 10:56:18 -0500

    Delete veterinaria.js

--- veterinaria.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elemsCarousel = document.querySelectorAll('.carousel');
-  M.Carousel.init(elemsCarousel, {
-    fullWidth: true,
-    indicators: true
-  });
-  var elemsSidenav = document.querySelectorAll('.sidenav');
-  M.Sidenav.init(elemsSidenav);
-});
